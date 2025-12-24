c55722e18914c2e95547499c4114ef28af701711
WDAGUtilityAccount <cantaloupe137@gmail.com>
2025-12-24 22:30:40 +0800

    更改邏輯

--- Dynamic Programming/0_1_knapback.cpp
+++ Dynamic Programming/0_1_knapback.cpp
@@ -6,41 +6,36 @@
 #define WA() ios::sync_with_stdio(0), cin.tie(0)
 #define int long long
 #define endl "\n"
+#define pb push_back
 using namespace std;
 
 const int MAXN = 105;
 const int MAXW = 10005;
 
 int dp[MAXN][MAXW];
-int v[MAXN], w[MAXN];
 
 signed main() {
     WA();
 
     string line1, line2;
+    vector<int>v{0}, w{0};
     int W_max;
 
     // 物品價值
     getline(cin, line1);
     stringstream ss1(line1);
-    int n = 0, val;
-    while (ss1 >> val) {
-        v[++n] = val; // 從index 1 開始存
-    }
+    int val;
+    while (ss1 >> val) v.pb(val);
 
     // 物品重量
     getline(cin, line2);
     stringstream ss2(line2);
-    for (int i = 1; i <= n; ++i) {
-        ss2 >> w[i];
-    }
+    int weight;
+    while (ss2 >> weight) w.pb(weight);
 
     // 背包可承受最大重量
     cin >> W_max;
-
-
-    // 初始化
-    for (int j = 0; j <= W_max; j++) dp[0][j] = 0;
+    int n = v.size() - 1;
 
     for (int i = 1; i <= n; i++) {
         for (int j = 0; j <= W_max; j++) {
