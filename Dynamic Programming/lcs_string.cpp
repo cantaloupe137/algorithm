15daed987a6bdf05e33818cd1ef535ca4b915125
WDAGUtilityAccount <cantaloupe137@gmail.com>
2025-12-30 14:56:06 +0800

    update lcs_string.cpp

--- Dynamic Programming/lcs_string.cpp
+++ Dynamic Programming/lcs_string.cpp
@@ -6,20 +6,21 @@ using namespace std;
 #define WA() ios::sync_with_stdio(false), cin.tie(0)
 #define endl '\n'
 int main() {
-    string a,b;
-    while (getline(cin,a) && getline(cin,b)) {
-        int dp[a.length()+1][b.length()+1];
-        for(int i=0;i<=a.length();i++)
-            dp[i][0]=0;
-        for(int i=0;i<=b.length();i++)
-            dp[0][i]=0;
-        for(int i=1;i<=a.length();i++)
-            for(int j=1;j<=b.length();j++) {
-                if(a[i-1]!=b[j-1])
-                    dp[i][j]=max(max(dp[i][j-1],dp[i-1][j]),dp[i-1][j-1]);
-                else
-                    dp[i][j]=dp[i-1][j-1]+1;
+    WA();
+    string a, b;
+    while (getline(cin, a) && getline(cin, b)) {
+        int n = a.length();
+        int m = b.length();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
-        cout << dp[a.length()][b.length()] << endl;
+        }
+        cout << dp[n][m] << endl;
     }
-}
+    return 0;
+    }
